35eec51b1f9ab1b4ee04ed12f59317bf9ee53c09
Chris Reed <chris@reedtech.us>
2023-11-01 18:22:15 -0500

    Added PostTag to test two word models/resources

---
+++ app/Filament/Resources/PostTagResource.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PostTagResource\Pages;
+use App\Filament\Resources\PostTagResource\RelationManagers;
+use App\Models\PostTag;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PostTagResource extends Resource
+{
+    protected static ?string $model = PostTag::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->autofocus()
+                    ->required()
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable()
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPostTags::route('/'),
+            'create' => Pages\CreatePostTag::route('/create'),
+            'edit' => Pages\EditPostTag::route('/{record}/edit'),
+        ];
+    }
+}
