b780800cf8b3aa5b04c7aa1b335422215dd3fe69
Chris Reed <chris@reedtech.us>
2023-11-01 17:28:34 -0500

    Updated packages and config

--- config/bastion.php
+++ config/bastion.php
@@ -2,7 +2,26 @@
 
 // config for ChrisReedIO/Bastion
 return [
+    'models' => [
+        'permission' => \Spatie\Permission\Models\Permission::class,
+        'role' => \Spatie\Permission\Models\Role::class,
+        'user' => '\App\Models\User',
+    ],
+
+    'permissions' => [
+        'preload' => true,
+
+    ],
+
+    'default_guard' => 'web',
+    'guards' => [
+        // value => 'Custom Label'
+        'web' => 'Web',
+        'api' => 'API',
+        // Your other custom guards here
+    ],
+
     'sso' => [
-        'enabled' => 1111
+        'enabled' => false,
     ],
 ];
